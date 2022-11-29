ad86d1343ebdcceed0acfa45f6cd5305f7f090c8
takuya_kajiwara <fd.elt.119@gmail.com>
2022-11-29 21:02:19 +0900

    first-commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Test;
+use Illuminate\Support\Facades\DB;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        dd('test');
+        //Eloquent(エロクアント)
+        $values = Test::all();
+
+        $count = Test::count();
+
+        $first = Test::findOrFail(1);
+
+        $where = Test::where('text', '=', 'bbb')->get();
+
+        //クエリビルダ
+        $queryBuilder = DB::table('tests')->where('text', '=', 'bbb')->select('id', 'text')->get();
+
+        // dd($values, $count, $first, $where, $queryBuilder);
+
+        return view('tests.test', compact('values'));
+    }
+}
---
+++ app/Services/CheckFormService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+class CheckFormService
+{
+    public static function checkGender($data)
+    {
+        if ($data->gender === 0) {
+            $gender = '男性';
+        } else {
+            $gender = '女性';
+        }
+        return $gender;
+    }
+
+    public static function checkAge($data)
+    {
+        if ($data->age === 1) {
+            $age = '〜１９歳';
+        }
+        if ($data->age === 2) {
+            $age = '２０歳〜２９歳';
+        }
+        if ($data->age === 3) {
+            $age = '３０歳〜３９歳';
+        }
+        if ($data->age === 4) {
+            $age = '４０歳〜４９歳';
+        }
+        if ($data->age === 5) {
+            $age = '５０歳〜５９歳';
+        }
+        if ($data->age === 6) {
+            $age = '６０歳〜';
+        }
+        return $age;
+    }
+}
